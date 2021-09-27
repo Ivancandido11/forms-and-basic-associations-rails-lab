e96dd139d0fb17f4604e24ff392c53427169ef94
Ivan Candido <ivan.candido@powerhrg.com>
2021-09-27 13:35:13 -0400

    Completed Assignment

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,26 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+    artist.songs << self
+  end
+
+  def artist_name
+    artist ? artist.name : nil
+  end
+
+  def note_contents=(arr_content)
+    arr_content.each do |content|
+      note = Note.find_or_create_by(content: content)
+      notes << note
+    end
+  end
+
+  def note_contents
+    notes ? notes.map(&:content) : nil
+  end
 end
